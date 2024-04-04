e1ee3316f6446a05542f9a582ae428e7709e2157
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-04-04 15:49:08 -0300

    feat: adicionando números

--- src/numeros/Numeros.java
+++ src/numeros/Numeros.java
@@ -1,4 +1,38 @@
 package numeros;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Numeros {
+    ArrayList<Integer> numeros;
+
+    public Numeros(Scanner scanner) {
+        this.numeros = new ArrayList<>();
+        pedirNumeros(scanner);
+    }
+
+    private void pedirNumeros(Scanner scanner) {
+        int num;
+
+        do {
+            System.out.println("Informe um número: ");
+            num = scanner.nextInt();
+            if (num >= 0) this.numeros.add(num);
+        } while (num >= 0);
+    }
+
+    public int quantidadeNumerosDigitados() {
+        return numeros.size();
+    }
+
+    public double mediaNumerosDigitados() {
+        double soma = 0;
+
+        for (double num: numeros) {
+            soma += num;
+        }
+
+        double media = soma / quantidadeNumerosDigitados();
+        return Math.round(media * 100.0) / 100.0;
+    }
 }
