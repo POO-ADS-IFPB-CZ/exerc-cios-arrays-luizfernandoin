eccc960aaf5d43b1cfe413203ca4553457433aff
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-04-04 15:48:23 -0300

    feat: adicionando interface

--- src/front/Interface.java
+++ src/front/Interface.java
@@ -1,4 +1,10 @@
 package front;
 
 public class Interface {
+    public static void menu(String ... mensagens) {
+        System.out.println("==========");
+        for (int index = 0; index < mensagens.length; index++) {
+            System.out.printf("[%d] %s \n", index + 1, mensagens[index]);
+        }
+    }
 }
